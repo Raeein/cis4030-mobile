32b7b228b7c4e807b4b229a70a9ae3fba1187fb5
ellamcivor <emcivorr@gmail.com>
2024-03-05 18:25:40 -0500

    location button

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,10 +1,29 @@
-import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { router } from 'expo-router';
+import Colors from "@/constants/Colors";
+
+function changeLocation() {
+
+}
+
+function LocationButton( ) {
+    return (
+        <TouchableOpacity style={styles.locationBtn} onPress={changeLocation}>
+            <Text style={styles.locBtnText}>Location</Text>
+            <Image style={styles.arrow} source={require('@/assets/images/down-arrow-icon.png')} />
+        </TouchableOpacity>
+    );
+}
 
 export default function HomeScreen() {
     return (
         <View style={styles.container}>
-        <Text style={styles.title}>Tab One</Text>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            {/* <ScrollView> */}
+                <View style={styles.locationBtnContainer}>
+                    <LocationButton></LocationButton>
+                </View>                
+            {/* </ScrollView> */}
         </View>
     );
 }
@@ -23,5 +42,28 @@ const styles = StyleSheet.create({
         marginVertical: 30,
         height: 1,
         width: '80%',
+    },
+    locationBtnContainer: {
+        top: -330,
+    },
+    locationBtn: {
+        backgroundColor: Colors.primary,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        marginVertical: 5,
+        minWidth: 200,
+        alignItems: 'center',
+        justifyContent: 'centers',
+        flexDirection: 'row',
+    },
+    locBtnText: {
+        fontSize: 18,
+        color: '#fff'
+    },
+    arrow: {
+        width: 20,
+        height: 20,
+        tintColor: 'ffffff',
     },
 });
